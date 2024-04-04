1adf1c034c40fe27b778242e7c1a961161bffbe2
uni-delphi <rojonicolasdev@gmail.com>
2024-04-04 13:33:13 -0300

    minor fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -17,33 +17,14 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   const session = await getServerSession(authOptions);
-  if (!session || !session.user) {
+  
+  /*if (!session || !session.user) {
     redirect("/");
-  }
+  }*/
   return (
     <>
       {/* Agregar navecaion del dashboard aca */}
       <div className="flex flex-col items-center gap-8 p-4 md:p-8 mx-auto w-full">
-        <nav className="w-full max-w-md flex justify-around mb-8">
-          <Link
-            className="text-lg font-semibold text-gray-700 hover:text-gray-900"
-            href="/dashboard"
-          >
-            Eventos
-          </Link>
-          <Link
-            className="text-lg font-semibold text-gray-700 hover:text-gray-900"
-            href="/dashboard/codigos"
-          >
-            Codigos de descuento
-          </Link>
-          <Link
-            className="text-lg font-semibold text-gray-700 hover:text-gray-900"
-            href="/dashboard/configuracion"
-          >
-            Configuración
-          </Link>
-        </nav>
         {children}
       </div>
       <Toaster />
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,30 +16,14 @@ import { getUserByEmail } from "@/lib/api/users";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
+  console.log("🚀 ~ Dashboard ~ session:", session)
   const { id } = await getUserByEmail(session?.user?.email as string);
+  console.log("🚀 ~ Dashboard ~ id:", id)
 
   return (
-    <>
-      <Button asChild variant="secondary">
-        <Link href={"/dashboard/nuevo-evento"}>
-          <Plus className="mr-2" /> Nuevo evento
-        </Link>
-      </Button>
-      <div className="flex flex-col items-center gap-8 p-4 md:p-8">
-        <div className="flex">
-          <Input type="text" placeholder="Buscar evento" />
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Ordenar por" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="light">Fecha asc</SelectItem>
-              <SelectItem value="dark">Fecha desc</SelectItem>
-              <SelectItem value="dark">Titulo</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        
+    <>      
+      <div className="flex flex-col items-center gap-8 p-4 md:p-8 ">
+                
       </div>
     </>
   );
