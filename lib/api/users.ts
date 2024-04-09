05d0c99fa1c7c5431c14b3e46b1d1206f386647f
uni-delphi <rojonicolasdev@gmail.com>
2024-04-09 12:49:21 -0300

    added methods

--- lib/api/users.ts
+++ lib/api/users.ts
@@ -1,7 +1,7 @@
 import { db } from "../prisma";
 
 export async function getUserByEmail(email: string) {
-  return await db.user.findFirstOrThrow({
+  return await db.user.findUnique({
     where: {
       email,
     },
@@ -17,12 +17,17 @@ export async function getUserById(userId: string) {
 }
 
 export async function createUser(data: any) {
-  console.log("🚀 ~ createUser ~ data:", data)
+  console.log("🚀 ~ createUser ~ data:", data);
   return await db.user.create({ data });
 }
 
+export async function logInUser(data: any) {
+  console.log("🚀 ~ createUser ~ data:", data);
+  return await db.user.findUnique({ where: { email: data.email } });
+}
+
 export async function updateUser(data: any, email: string) {
-  const { id } = await getUserByEmail(email as string);
+  const { id } : any = await getUserByEmail(email as string);
   return await db.user.update({
     where: {
       id,
