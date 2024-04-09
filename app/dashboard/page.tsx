d0df83e42f868097661f03d9282d9d5b39096700
uni-delphi <rojonicolasdev@gmail.com>
2024-04-09 12:48:01 -0300

    minor fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,10 @@
-
+"use client"
 import { getServerSession } from "next-auth";
 import Link from "next/link";
-import { authOptions } from "@/utils/auth-options";
+import { authOptions } from "@/auth.config";
 import { getUserByEmail } from "@/lib/api/users";
+import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
 
 export default async function Dashboard() {
   /*const session = await getServerSession(authOptions);
@@ -14,6 +16,10 @@ export default async function Dashboard() {
     <>
       <div className="flex flex-col items-center gap-8 p-4 md:p-8 ">
         Dashboard
+        <Button onClick={() => signOut({
+          redirect: true,
+          callbackUrl: '/'
+        })}>sign out</Button>
       </div>
     </>
   );
