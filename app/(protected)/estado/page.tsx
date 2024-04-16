9c8ca1d5571d271d5ea1d8a0d72238097d9adc00
uni-delphi <rojonicolasdev@gmail.com>
2024-04-16 11:44:50 -0300

    updated estado page

--- app/(protected)/estado/page.tsx
+++ app/(protected)/estado/page.tsx
@@ -1,15 +1,19 @@
 import { authOptions } from "@/auth.config";
 
 import NavBar from "@/components/nav-bar/nav-bar";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 import { TUser } from "@/types/user";
 import { Session, getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 export default async function Encuestas() {
-  const session = await getServerSession(authOptions);  
-  if (!session || !session.user) redirect("/");
   
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) redirect("/");
+  const { name } = session.user;
+
   return (
     <main className="">
       <NavBar
@@ -18,15 +22,107 @@ export default async function Encuestas() {
         session={session as Session}
       />
 
-      <div className="overflow-hidden">
-        <h1 style={{
-        marginTop: "8rem",
-      }}>Esta es estado</h1>
-        <hr />
-        <h2 className="pt-20 mt-5 pb-2 text-center mx-auto">
-          Uso y aplicación de tecnologías 3D en producción y mantenimiento de
-          repuestos o componentes
-        </h2>
+      <div className="grid grid-cols-1 xl:grid-cols-2 h-screen">
+        <section className="w-full">
+          <Image
+            src={"/gente.jpg"}
+            alt="image"
+            width={200}
+            height={160}
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+            style={{ height: "100vh", width: "100%", objectFit: "cover" }}
+          />
+        </section>
+        <section className="w-full overflow-y-auto px-4 text-textColor my-4">
+          <div>
+            <Image
+              src={"/logos-unc.png"}
+              alt="image"
+              width={500}
+              height={160}
+              style={{
+                height: "auto",
+                width: "100%",
+                objectFit: "cover",
+                padding: "0 200px",
+              }}
+            />
+            <h2 className="font-bold mx-20 mt-10 text-2xl ">
+              <span className="block line-clamp-2">Hola {name}!</span>
+              <span className="block line-clamp-2">
+                Tu contribución a *Nombre del estudio* es del 83%
+              </span>
+            </h2>
+            <div className="max-w-[80%] mx-auto mt-1">
+              <p className="mb-4">
+                Puedes volver a completar, ampliar o modificar la justifiacion
+                de tus respuestas.
+              </p>
+              <p className="pb-4 mb-4">
+                A continuacion te mostraremos el estado de tu encuesta.
+              </p>
+              <p className="pb-4 mb-4">
+                Al estudio le restan nn días para finalizar
+              </p>
+            </div>
+          </div>
+          <div className="max-w-3xl mx-auto">
+            <div className="my-4">
+              <h1 className="text-2xl font-bold mb-4">Tecnología 1</h1>
+              <div className="grid ">
+                <div className="bg-[#CCE8D4] shadow-md rounded-lg p-4 flex items-center justify-between">
+                  <p className="text-gray-800 font-semibold">Completa</p>
+                  <p className="text-gray-600">Enunciado 1</p>
+                  <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Ampliar Respuesta
+                  </Button>
+                </div>
+                <div className="bg-[#FFFFBF] shadow-md rounded-lg p-4 flex cols-12 items-center justify-between">
+                  <p className="text-gray-800 font-semibold">Casi Listo</p>
+                  <p className="text-gray-600">Enunciado 2</p>
+                  <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Continuar
+                  </Button>
+                </div>
+                <div className="bg-[#EAEAEA] shadow-md rounded-lg p-4 flex cols-12 items-center justify-between">
+                  <p className="text-gray-800 font-semibold">Por Empezar</p>
+                  <p className="text-gray-600">Enunciado 3</p>
+                  <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Empezar Ahora
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className="my-4">
+              <h1 className="text-2xl font-bold mb-4">Tecnología 2</h1>
+              <div className="grid ">
+                <div className="bg-[#FFFFBF] shadow-md rounded-lg p-4 flex cols-12 items-center justify-between">
+                  <p className="text-gray-800 font-semibold">Casi Listo</p>
+                  <p className="text-gray-600">Enunciado 1</p>
+                  <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Ampliar Respuesta
+                  </Button>
+                </div>
+                <div className="bg-[#CCE8D4] shadow-md rounded-lg p-4 flex items-center justify-between">
+                  <p className="text-gray-800 font-semibold">Completa</p>
+                  <p className="text-gray-600">Enunciado 2</p>
+                  <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Continuar
+                  </Button>
+                </div>
+                <div className="bg-[#EAEAEA] shadow-md rounded-lg p-4 flex cols-12 items-center justify-between">
+                  <p className="text-gray-800 font-semibold">Por Empezar</p>
+                  <p className="text-gray-600">Enunciado 3</p>
+                  <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Empezar Ahora
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   );
