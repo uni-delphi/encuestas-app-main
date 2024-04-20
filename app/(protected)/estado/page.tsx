b99fa1115757542016c5d3b158973529ac20f2af
uni-delphi <rojonicolasdev@gmail.com>
2024-04-20 20:01:42 -0300

    added href slug

--- app/(protected)/estado/page.tsx
+++ app/(protected)/estado/page.tsx
@@ -89,7 +89,7 @@ export default async function Encuestas() {
                           </p>
                           <div className="flex-auto w-full py-2 md:py-0 md:w-1/3 text-right">
                             <Link
-                              href={``}
+                              href={`/${tecnologia.slug}/${enunciado.slug}`}
                               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                             >
                               Responder
