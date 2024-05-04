1efdcb15439a0c1f6adcd30e8b05a624a313481e
cuerojr <cannibal5033@gmail.com>
2024-05-04 10:51:40 -0300

    added type

---
+++ types/respuestas.ts
@@ -0,0 +1,17 @@
+export type TRESPONSE = {
+    technology?: string;
+    enunciado?: string;
+    question?: string;
+    createdAt: Date;
+    checkboxChoises?: string;
+    respuestas?: string;
+    respondentName?: string;
+    respondentEmail: string;
+    respondentCountry: string;
+    respondentState: string;
+    respondentEducation: string;
+    respondentSector: string;
+    respondentInstitution: string;
+    respondentExpertees: string;
+    respondentYears: string
+  };
