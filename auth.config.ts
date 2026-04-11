f65a2ac01306faa83aaf1d29f7bb752474aaa158
cuerojr <cannibal5033@gmail.com>
2026-04-11 02:44:05 -0300

    sd

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,6 @@
 import { Account, NextAuthOptions, User } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@/generated/prisma/client";
+
 import bcrypt from "bcrypt";
 import { prisma }  from "@/lib/prisma";
 
@@ -8,7 +8,9 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma as unknown as PrismaClient),
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
