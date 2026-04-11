c3171bd59af5e628bf4bd85e000fdb758ec8ae4b
cuerojr <cannibal5033@gmail.com>
2026-04-11 02:13:31 -0300

    asd

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,12 +4,15 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+const isProd = process.env.NODE_ENV === "production";
+const dbUrl = isProd ? env("POSTGRES_URL") : env("DATABASE_URL");
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: dbUrl,
   },
 });
