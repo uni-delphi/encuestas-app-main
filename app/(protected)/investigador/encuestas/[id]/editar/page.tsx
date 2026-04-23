f6be107060f135bbe387460ea0c7fc0f28d28db9
cuerojr <cannibal5033@gmail.com>
2026-04-23 15:46:16 -0300

    asd

--- app/(protected)/investigador/encuestas/[id]/editar/page.tsx
+++ app/(protected)/investigador/encuestas/[id]/editar/page.tsx
@@ -1,14 +1,16 @@
 import SurveyMain from "@/components/survey/survey-main";
+import { getEncuestaById } from "@/lib/actions";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const data = await params;
+    const surveyData: any = await getEncuestaById(Number(data.id));
   console.log("🚀 ~ Page ~ params:", data);
 
   return (
     <section className="px-10 py-20">
       Aca va el formulario para editar la encuesta {data.id}
       <div>
-        <SurveyMain />
+        <SurveyMain encuesta={surveyData}/>
       </div>
     </section>
   );
